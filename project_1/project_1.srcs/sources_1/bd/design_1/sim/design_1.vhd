1819009e4fe620c2c12fc5105dded83b49557f85
Luke Allen <lgallen95@gmail.com>
2020-04-28 21:30:29 -0400

    Added VHDL wrapper for block diagram

---
+++ project_1/project_1.srcs/sources_1/bd/design_1/sim/design_1.vhd
@@ -0,0 +1,36 @@
+--Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+--Date        : Tue Apr 28 21:28:01 2020
+--Host        : Luke-PC running 64-bit major release  (build 9200)
+--Command     : generate_target design_1.bd
+--Design      : design_1
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1 is
+  attribute CORE_GENERATION_INFO : string;
+  attribute CORE_GENERATION_INFO of design_1 : entity is "design_1,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=design_1,x_ipVersion=1.00.a,x_ipLanguage=VHDL,numBlks=1,numReposBlks=1,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,synth_mode=OOC_per_IP}";
+  attribute HW_HANDOFF : string;
+  attribute HW_HANDOFF of design_1 : entity is "design_1.hwdef";
+end design_1;
+
+architecture STRUCTURE of design_1 is
+  component design_1_zynq_ultra_ps_e_0_0 is
+  port (
+    pl_resetn0 : out STD_LOGIC;
+    pl_clk0 : out STD_LOGIC
+  );
+  end component design_1_zynq_ultra_ps_e_0_0;
+  signal NLW_zynq_ultra_ps_e_0_pl_clk0_UNCONNECTED : STD_LOGIC;
+  signal NLW_zynq_ultra_ps_e_0_pl_resetn0_UNCONNECTED : STD_LOGIC;
+begin
+zynq_ultra_ps_e_0: component design_1_zynq_ultra_ps_e_0_0
+     port map (
+      pl_clk0 => NLW_zynq_ultra_ps_e_0_pl_clk0_UNCONNECTED,
+      pl_resetn0 => NLW_zynq_ultra_ps_e_0_pl_resetn0_UNCONNECTED
+    );
+end STRUCTURE;
